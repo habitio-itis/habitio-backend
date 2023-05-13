b87f15295b745a1da035b5336136340ba4ec43bb
Aydar Ibragimov <iam@renue.ru>
2023-05-13 22:43:23 +0300

    Init commit

---
+++ src/main/java/ru/itis/habitio/HabitioBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.habitio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/habitio/HabitioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.habitio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
